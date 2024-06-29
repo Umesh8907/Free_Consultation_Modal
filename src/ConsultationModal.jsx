8bd02918c27d88257c198ac04e76e39df6ce588a
Umesh8907 <umesh.tandon.51@gmail.com>
2024-06-29 16:45:41 +0530

    publick

--- src/ConsultationModal.jsx
+++ src/ConsultationModal.jsx
@@ -14,12 +14,11 @@ const ConsultationModal = () => {
   const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
 
   useEffect(() => {
-   
     const timeout = setTimeout(() => {
       setIsOpen(true);
     }, 3000);
 
-    return () => clearTimeout(timeout); 
+    return () => clearTimeout(timeout);
   }, []);
 
   useEffect(() => {
@@ -103,31 +102,30 @@ const ConsultationModal = () => {
     switch (step) {
       case 1:
         return (
-          <div className="Main">
+          <div className="Main ">
             <div className="flex ">
-              <div className="left w-1/2">
-                <h1>left</h1>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aspernatur inventore natus accusamus, reiciendis beatae iusto.
-                  Deserunt earum voluptate accusantium a.
-                </p>
+              <div className="left w-1/2 ">
+                <img
+                  src="/elements/doctor.png"
+                  alt="banner"
+                  className=" w-[380px] "
+                />
               </div>
-              <div class="border-l border-gray-400 px-5"></div>
-              <div className="righ w-1/2 flex flex-col gap-4 p-4">
-                <h2 className="text-[#B955AA] text-lg font-bold">
+              <div class="flex border-l border-gray-400 px-5 h-[400px] my-auto"></div>
+              <div className="righ w-1/2 flex flex-col gap-6 justify-center mx-10">
+                <h2 className="text-[#B955AA] text-[22px] font-bold">
                   Book a Free Consultation{" "}
                 </h2>
-                <h1 className="text-[#652AB6] font-bold text-[38px] leading-tight">
+                <h1 className="text-[#652AB6] font-extrabold text-[47.6px] leading-tight">
                   Unlock Your Path to Wellness
                 </h1>
-                <p className="text-gray-700 text-sm">
+                <p className="text-gray-700 text-[14px]">
                   Are you ready to take the first step towards better health and
                   well-being? Our free consultation will connect you with the
                   right expert on Infano.Care.
                 </p>
                 <button
-                  className="w-3/4 py-4 px-2 bg-[#652AB6] text-white rounded-lg"
+                  className="w-3/4 py-4 px-2 bg-[#652AB6] text-white rounded-lg text-lg"
                   onClick={handleNext}
                 >
                   Start Your Free Consultation
@@ -138,16 +136,21 @@ const ConsultationModal = () => {
         );
       case 2:
         return (
-          <div className="grid grid-cols-1 gap-4">
-            <h1>lets choose your answer</h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam,
-              magni! Dicta ipsum exercitationem illum, tempora placeat
-              voluptatum dolor vero ipsa.
+          <div className="m-10 flex flex-col gap-8 p-10">
+            <h1 className="text-[35px] font-extrabold text-[#652AB6] ">
+              Welcome to Infano.Care
+            </h1>
+            <p className="text-[18px] leading-8  ">
+              We're thrilled you're here! To provide you with the best possible
+              experience, we need to learn a bit more about you. This short
+              questionnaire will help us understand your specific needs and
+              match you with the perfect expert.
             </p>
-            <div className="flex justify-between mt-4">
+            <div className="flex items-center justify-between mt-10">
+              {" "}
+              <p>92% People Feel Healthier With A Holistic Health Approach</p>
               <button
-                className="py-2 px-4 bg-blue-500 text-white rounded"
+                className="py-2 px-8 bg-[#652AB6] text-white rounded-lg text-lg"
                 onClick={handleNext}
               >
                 Next
@@ -157,26 +160,33 @@ const ConsultationModal = () => {
         );
       case 3:
         return (
-          <div className="grid grid-cols-1 gap-4">
-            {consultation_data.map((category) => (
-              <div
-                key={category.id}
-                className={`p-4 border rounded cursor-pointer hover:bg-gray-200 ${
-                  selectedCategory?.id === category.id ? "bg-gray-300" : ""
-                }`}
-                onClick={() => handleCategorySelect(category)}
-              >
-                {category.category}
-              </div>
-            ))}
-            <div className="flex justify-between mt-4">
-              <button
-                className="py-2 px-4 bg-blue-500 text-white rounded"
-                onClick={handleNext3}
+          <div className="flex flex-col gap-10 p-20 relative">
+            <h1 className="text-[35px] font-extrabold text-[#652AB6]">
+              What aspect of your health would you like to discuss during this
+              consultation?
+            </h1>
+            <div className="w-[50%] grid grid-cols-2 gap-2 ">
+              {consultation_data.map((category) => (
+                <div
+                  key={category.id}
+                  className={`border p-4 rounded-full text-center text-white cursor-pointer ${
+                    selectedCategory?.id === category.id
+                      ? " borde-2 border-[#B955AA] text-black"
+                      : " bg-[#B955AA]"
+                  }`}
+                  onClick={() => handleCategorySelect(category)}
+                >
+                  {category.category}
+                </div>
+              ))}
+            </div>
+           
+            <button
+                className="absolute bottom-0 right-0 py-2 px-8 bg-[#652AB6] text-white rounded-lg text-lg  "
+                onClick={handleNext}
               >
                 Next
               </button>
-            </div>
           </div>
         );
       case 4:
@@ -358,8 +368,8 @@ const ConsultationModal = () => {
         Open Consultation Modal
       </button>
       {isOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-2xl shadow-lg w-11/12 md:w-1/2 relative">
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center ">
+          <div className="bg-white  rounded-2xl shadow-lg  relative w-[950px]">
             <button
               className="absolute top-2 right-2 text-gray-700 hover:text-gray-900"
               onClick={handleClose}
